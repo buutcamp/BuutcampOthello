8425577bc0448646896dd744ce40406458985e9d
JuKaNo <juha.nord@mail.suomi.net>
2020-06-09 14:30:04 +0300

    Disk placement and flipping fixed.

--- src/othello.h
+++ src/othello.h
@@ -17,11 +17,14 @@
 #include <examples/imgui_impl_opengl2.h>
 #include <string>
 
+#define USE_HINT_MASK           0       //1 = Used in game, 0 = Not in use
+#define USE_DEBUG               1       //1 = In use, 0 = Not used
+
 /*
  * 
  */
 
-enum pcs {Empty, White, Black};
+enum pcs {Empty, White, Black, Hint};
 
 class Game {
     public: 
@@ -35,11 +38,15 @@ class Game {
         void OthelloRender(int width, int height);
         void OnTileClicked(int y, int x);
         void update();
-        
+
         bool OthelloButton(int x, int y);
-        int testPosition(const int x, const int y);
-       
-        
+        int  TestDirection(const int x, const int y, const int dir_x, const int dir_y);
+        int  TestPosition(const int x, const int y);
+        void FlipDisks(const int x, const int y);
+        #if (USE_HINT_MASK == 1)
+        void UpdateHintMask(void);
+        #endif
+
         const int diskRadius;
         const int tileSize;
         const int boardTiles;
@@ -56,8 +63,19 @@ class Game {
     private:
         int CurrentDiskColor;
         int GameBoard[8][8]; //int GameBoard[boardTiles][boardTiles];
+        #if (USE_HINT_MASK == 1)
+        int HintMask[8][8];
+        #endif
         SDL_Window* window;
         SDL_GLContext gl_context;
 };
+
+#if (USE_DEBUG == 1)
+#include <iostream>
+void dbMessage(const std::string &s, bool crlf);
+#endif
+//#if (USE_DEBUG == 1)
+//dbMessage(const std::string &s, bool crlf);
+//#endif
 
 #endif      //end othello.h
