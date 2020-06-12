f6154a919979a48da1c877daefd107f7f0fa1b7a
buutcamp <4haneta@gmail.com>
2020-06-12 09:19:41 +0300

    Memory cleanup function added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 /*#define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include <SDL_opengl.h>
@@ -16,24 +15,18 @@
 int main(void)
 {
     Game game(White);
+
     game.InitSdl();
     game.InitImgui();
-
     game.OthelloInit();
 
-    uint64_t ticksLast = SDL_GetPerformanceCounter();
-
-    SDL_Event event;
-    while (true)
+    while (game.gameRunning())
     {
-        while (SDL_PollEvent(&event))
-        {
-            // SDL events are passed to imgui
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                return 0;
-        }
+        game.handleEvents();
         game.update();
     }
-	return 0;
+
+    //clean up everything
+    //game.clean();
+    return 0;
 }
